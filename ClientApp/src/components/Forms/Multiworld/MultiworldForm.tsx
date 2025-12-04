f93d6f96ddcec70ae5ee501929e4f53ee3b235b1
Spiffy <spiffyzeroseven@gmail.com>
2025-12-04 00:08:30 -0600

    fixed default parameter of "session"

--- ClientApp/src/components/Forms/Multiworld/MultiworldForm.tsx
+++ ClientApp/src/components/Forms/Multiworld/MultiworldForm.tsx
@@ -39,7 +39,7 @@ export default function MultiworldForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      session: "",
+      session: "0",
       username: "",
       additionalComments: "",
     },
@@ -66,7 +66,7 @@ export default function MultiworldForm() {
                   <FieldLabel>Multiworld Session</FieldLabel>
                   <Select
                     name={field.name}
-                    value={field.value = "0"}
+                    value={field.value}
                     onValueChange={field.onChange}
                   >
                     <SelectTrigger aria-invalid={fieldState.invalid}>
