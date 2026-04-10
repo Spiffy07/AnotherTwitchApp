882d2ad5d4794c8dd7c4f8c38d79f863f35a5c62
Spiffy <spiffyzeroseven@gmail.com>
2026-04-10 01:06:02 -0500

    added glass effect behind 'Projects' for readability. add several testing backgrounds (ended with gradientblur.png)

--- ClientApp/src/components/ProjectsSection.jsx
+++ ClientApp/src/components/ProjectsSection.jsx
@@ -23,52 +23,54 @@ export default function ProjectsSection() {
   // I want this to be a grid format with demo/preview images that have hover animations
   return (
     <>
-      <h1 className="text-6xl text-center m-4">Projects</h1>
-      <div className="w-5xl mx-auto grid grid-cols-2 gap-16 m-2">
-        {/* Project 1 this website, not just the portfolio page */}
-        <div className="text-center m-4">
-          <h3 className="text-2xl">Twitch WebApp</h3>
-          <p> Description here</p>
+      <div className='bg-slate-900/40 backdrop-blur-md rounded-xl '>
+        <h1 className="text-6xl text-center m-4">Projects</h1>
+        <div className="w-5xl mx-auto grid grid-cols-2 gap-16 m-2 ">
+          {/* Project 1 this website, not just the portfolio page */}
+          <div className="text-center m-4">
+            <h3 className="text-2xl">Twitch WebApp</h3>
+            <p> Description here</p>
+          </div>
+          <Card
+            className={`min-w-xs h-auto hover:cursor-pointer ${cardStyle} origin-right hover:scale-120`}
+            onClick={() => {
+              window.open(urlTwitchApp, "_blank");
+            }}
+          >
+            <img src={demoTwitchApp} />
+          </Card>
+          {/* Project 2 */}
+          <Card
+            className={`min-w-xs h-auto hover:cursor-pointer ${cardStyle} origin-left hover:scale-120`}
+            onClick={() => {
+              window.open(urlBrokeBot, "_blank");
+            }}
+          >
+            <img src={demoBrokeBot} />
+          </Card>
+          <div className="text-center m-4">
+            <h3 className="text-2xl font-sans">
+              Interactive Twitch Bot "BrokeBot"
+            </h3>
+            <p> Description here</p>
+          </div>
+          {/* Project 3 */}
+          <div className="text-center m-4">
+            <h3 className="text-2xl font-sans">Unreal Engine 5 Castle"</h3>
+            <p> Description here</p>
+          </div>
+          <Card
+            className={`min-w-xs h-auto hover:cursor-pointer ${cardStyle} origin-right hover:scale-120`}
+            onClick={() => {
+              window.open(
+                "https://github.com/Spiffy07/CastleEnvironment",
+                "_blank",
+              );
+            }}
+          >
+            <YtVideoPreview ytVideoId={demoCastleId} startTimeInSeconds={155} />
+          </Card>
         </div>
-        <Card
-          className={`min-w-xs h-auto hover:cursor-pointer ${cardStyle} origin-right hover:scale-120`}
-          onClick={() => {
-            window.open(urlTwitchApp, "_blank");
-          }}
-        >
-          <img src={demoTwitchApp} />
-        </Card>
-        {/* Project 2 */}
-        <Card
-          className={`min-w-xs h-auto hover:cursor-pointer ${cardStyle} origin-left hover:scale-120`}
-          onClick={() => {
-            window.open(urlBrokeBot, "_blank");
-          }}
-        >
-          <img src={demoBrokeBot} />
-        </Card>
-        <div className="text-center m-4">
-          <h3 className="text-2xl font-sans">
-            Interactive Twitch Bot "BrokeBot"
-          </h3>
-          <p> Description here</p>
-        </div>
-        {/* Project 3 */}
-        <div className="text-center m-4">
-          <h3 className="text-2xl font-sans">Unreal Engine 5 Castle"</h3>
-          <p> Description here</p>
-        </div>
-        <Card
-          className={`min-w-xs h-auto hover:cursor-pointer ${cardStyle} origin-right hover:scale-120`}
-          onClick={() => {
-            window.open(
-              "https://github.com/Spiffy07/CastleEnvironment",
-              "_blank",
-            );
-          }}
-        >
-          <YtVideoPreview ytVideoId={demoCastleId} startTimeInSeconds={155} />
-        </Card>
       </div>
     </>
   );
