129104bc76ab4f9ffda246de7ccd435fc2e24206
Spiffy <spiffyzeroseven@gmail.com>
2026-05-01 23:11:56 -0500

    refactor footer to a Footer.jsx  component.
    install npm package motion (formerly framer-motion)

---
+++ ClientApp/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer
+      className="col-span-12 w-full text-center text-sm pt-6 pb-12 -mb-12 
+                   bg-linear-to-r from-gray-950/0 via-slate-900/80 to-gray-950/0 backdrop-blur-sm"
+    >
+      <p>Copyright © 2026 Thomas T.</p>
+      <p>All Rights Reserved</p>
+    </footer>
+  );
+}
