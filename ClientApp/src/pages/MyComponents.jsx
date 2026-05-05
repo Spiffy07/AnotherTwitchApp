addaf2d8a5cd7c140eb8eeebb762c6f89126dd7a
Spiffy <spiffyzeroseven@gmail.com>
2026-05-04 19:09:39 -0500

    refactor: convert MyComponents.js class component to MyComponents.jsx functional component

---
+++ ClientApp/src/pages/MyComponents.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { motion, useScroll, useTransform } from 'motion';
+
+import { Button } from "@/components/ui/button"
+import MyComponent from '../components/Test';
+import Game from '../components/TicTacToeTut/TicTacToeTut';
+import GameTs from '../components/TicTacToeTut/TicTacToeTs';
+import MyProductTable from '../components/ProductTableTutorial/ProductTable';
+
+export function MyComponents() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  }
+
+    return (
+      <div>
+        <h1>Counter</h1>
+
+        <p>This is a simple example of a React component.</p>
+
+        <p aria-live="polite">Current count: <strong>{count}</strong></p>
+
+        <Button variant="secondary" onClick={incrementCounter}>Increment</Button>
+        <br/>
+        <br/>
+        <MyComponent />
+        <br/>
+        <Game />
+        <br/>
+        <GameTs />
+        <br/>
+        <MyProductTable/>
+      </div>
+    );
+  }
+
